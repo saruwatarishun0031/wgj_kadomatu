e63580779e56fdf629eb8e04eb420b5f2d27df68
Aiba_0325 <bantan.ab325@gmail.com>
2022-12-22 14:18:26 +0900

    [竹を切る]

--- Assets/Aiba/Scripts/TakeCut.cs
+++ Assets/Aiba/Scripts/TakeCut.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 public class TakeCut : MonoBehaviour
 {
@@ -14,8 +15,12 @@ public class TakeCut : MonoBehaviour
     [Header("ピッタリ切った事にする誤差の許容")]
     [SerializeField] float _parfectCutGosa;
 
-    [Header("ピッタリの竹のオブジェクト")]
+    [Header("ピッタリ切った時の竹のオブジェクト")]
     [SerializeField] GameObject _parfectCutTake;
+
+    [Header("切らなかった時の竹のオブジェクト")]
+    [SerializeField] GameObject _noCutTake;
+
 
     [Header("切った後の誤差。ある竹の素材の多さによる")]
     [SerializeField] List<float> _distance = new List<float>();
@@ -26,9 +31,16 @@ public class TakeCut : MonoBehaviour
     [Header("下の方を切った時の竹のオブジェクト。誤差の少ないバージョンから入れる")]
     [SerializeField] List<GameObject> _MinussTake = new List<GameObject>();
 
+    [Header("強制終了させる時間")]
+    [SerializeField] float _endTime;
+
+    float _countTime = 0;
 
 
+    [Header("竹を切るボタン")]
+    [SerializeField] GameObject _cutButtun;
 
+    [SerializeField] SpownTake _spownTake;
     [SerializeField] GameManager _gm;
 
     void Start()
@@ -39,11 +51,27 @@ public class TakeCut : MonoBehaviour
 
     void Update()
     {
+        if(_spownTake.IsSpown)
+        {
+            _countTime += Time.deltaTime;
+            if(_countTime>=_endTime)
+            {
+                _cutButtun.SetActive(false);
 
+                //出した竹を消す
+                var take = GameObject.FindGameObjectWithTag(_takeTagName);
+                Destroy(take);
+
+                _gm.TakesSet(_noCutTake);
+                SceneManager.LoadScene("Aiba");
+            }
+        }
     }
 
     public void Cut()
     {
+        _cutButtun.SetActive(false);
+
         //切り口と元の場所の差を求める
         var checkPointTake = GameObject.FindGameObjectWithTag(_takeCatPositionTagName);
        　//切り口との差
@@ -56,7 +84,8 @@ public class TakeCut : MonoBehaviour
         //パーフェクトかどうか求める
         if (distanceOfCutToCheckPoint < _parfectCutGosa && distanceOfCutToCheckPoint > -_parfectCutGosa)
         {
-
+            _gm.TakesSet(_parfectCutTake);
+            SceneManager.LoadScene("Aiba");
             return;
         }
 
@@ -71,6 +100,7 @@ public class TakeCut : MonoBehaviour
             }
             //一番誤差の小さいインデックス
             int num = go.IndexOf(go.Min());
+            _gm.TakesSet(_PlussTake[num]);
         }
         //切ったのが下のほうだったら
         else if (distanceOfCutToCheckPoint < -_parfectCutGosa)
@@ -83,9 +113,10 @@ public class TakeCut : MonoBehaviour
             }
             //一番誤差の小さいインデックス
             int num = go.IndexOf(go.Min());
+            _gm.TakesSet(_PlussTake[num]);
         }
 
-
+        SceneManager.LoadScene("Aiba");
     }
 
 
